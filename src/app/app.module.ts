b0cff566d66764b78811d1f3a6f23f9e25526c17
Daniel Cardoso <daocsilva@gmail.com>
2021-08-27 10:18:03 -0300

    Adicionado função de Data e Hora em tempo real.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { FizzBuzzComponent } from './componentes/fizz-buzz/fizz-buzz.component';
 import { NumeroDirective } from './diretivas/numero.directive';
 import { RetornoPipe } from './filtros/retorno.pipe';
+import { DataPipe } from './filtros/data.pipe';
+import { HoraPipe } from './filtros/hora.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     FizzBuzzComponent,
     NumeroDirective,
-    RetornoPipe
+    RetornoPipe,
+    DataPipe,
+    HoraPipe
   ],
   imports: [
     BrowserModule
